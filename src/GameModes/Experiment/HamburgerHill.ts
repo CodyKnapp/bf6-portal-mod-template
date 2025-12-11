dacd3bb3060ea90c0d02d1216a369798811b6b32
Cody Knapp <wcodyknapp@gmail.com>
2025-12-10 20:36:15 -0500

    Adding notes for what to handle later

---
+++ src/GameModes/Experiment/HamburgerHill.ts
@@ -0,0 +1,8 @@
+import { Core_AGameMode } from '../../Core/AGameMode';
+
+export class HamburgerHill extends Core_AGameMode {
+  // Initialize all capture points to 10 second capture / neutralize
+  // Make middle spawn point 15 cap/neutralize
+  // Adjust scoring so no points are scored unless the middle zone is held
+  // Each death = tickets - number of zones held
+}
