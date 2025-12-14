fc476028a4d19b17a113755a5517f315a3df299e
Cody Knapp <wcodyknapp@gmail.com>
2025-12-13 22:54:41 -0500

    minor refactor - adding game mode hooks

--- src/GameModes/Experiment/FulcrumProtocol.ts
+++ src/GameModes/Experiment/FulcrumProtocol.ts
@@ -1,15 +1,40 @@
-import { Core_AGameMode } from '../../Core/AGameMode';
+import {Core_AGameMode} from '../../Core/AGameMode';
 import * as modlib from 'modlib';
 import CapturePoint = mod.CapturePoint;
 import Team = mod.Team;
+import ScoreboardType = mod.ScoreboardType;
 
 export class FulcrumProtocol extends Core_AGameMode {
   private readonly STARTING_SCORE = 600;
+  private readonly FULCRUM_ID = 601;
   private capturePoints;
   private fulcrumPoint;
   private teams;
 
   onGameModeStarted(): void {
+    this.initializeObjectives();
+    this.initializeScoreboard();
+
+    this.teams = [
+      mod.GetTeam(1),
+      mod.GetTeam(2)
+    ].map(this.initializeTeam);
+
+    mod.SetGameModeScore(mod.GetTeam(1), this.STARTING_SCORE);
+    mod.SetGameModeScore(mod.GetTeam(2), this.STARTING_SCORE);
+  }
+
+  onPlayerDeployed(eventPlayer: mod.Player) {
+    // Remove tickets based on fulcrum ownership or not
+    // Adjust scoring so no points are scored unless the middle zone is held
+    // Each death = tickets - number of zones held
+  }
+
+  onPlayerDied(eventPlayer: mod.Player, eventOtherPlayer: mod.Player, eventDeathType: mod.DeathType, eventWeaponUnlock: mod.WeaponUnlock) {
+
+  }
+
+  private initializeObjectives() {
     this.capturePoints = [
       mod.GetCapturePoint(602),
       mod.GetCapturePoint(603),
@@ -17,20 +42,9 @@ export class FulcrumProtocol extends Core_AGameMode {
       mod.GetCapturePoint(605)
     ].map(this.initializeCapturePoint);
 
-    this.fulcrumPoint = mod.GetCapturePoint(601);
+    this.fulcrumPoint = mod.GetCapturePoint(this.FULCRUM_ID);
     this.initializeFulcrum(this.fulcrumPoint);
-
-    this.teams = [mod.GetTeam(1), mod.GetTeam(2)].map(this.initializeTeam);
-
-    mod.SetGameModeScore(mod.GetTeam(1), this.STARTING_SCORE);
-    mod.SetGameModeScore(mod.GetTeam(2), this.STARTING_SCORE);
   }
-
-  // Adjust scoring so no points are scored unless the middle zone is held
-  // Each death = tickets - number of zones held
-
-
-
 
   private initializeCapturePoint(point: CapturePoint): CapturePoint {
     mod.EnableCapturePointDeploying(point, true);
@@ -58,5 +72,9 @@ export class FulcrumProtocol extends Core_AGameMode {
     mod.SetGameModeScore(team, startingScore);
 
     return team;
+  }
+
+  private initializeScoreboard(): void {
+    mod.SetScoreboardType(ScoreboardType.CustomTwoTeams);
   }
 }
